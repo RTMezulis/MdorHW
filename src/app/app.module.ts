6fe9c6384d5b8690ca028d9ab80f7f209758f61b
Snakus <rudolfs.mezulis@gmail.com>
2021-06-10 18:26:14 +0300

    Added Clock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {RouterModule} from "@angular/router";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ClockComponent } from './components/clock/clock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ClockComponent
   ],
   imports: [
     BrowserModule,
